dd161992bdf8034aac145d6c82a56d1c067ee83f
nayan458 <nayancoding@gmail.com>
2025-11-07 12:45:02 +0530

    CSRF issue resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
-import { useCsrf } from "./hooks/useCsrf";
 
 function App() {
-  useCsrf();
   return (
     <>
       <RouterProvider router={router}/>
