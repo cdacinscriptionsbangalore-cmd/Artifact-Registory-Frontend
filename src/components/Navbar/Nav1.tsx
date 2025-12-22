71f80dfab97a45fa2c51334b842f396843ec00cc
sarthaksam003 <sarthaksam003@gmail.com>
2025-12-22 12:15:37 +0530

    Fixed styling after the navbar was fixed for authentication issues

--- src/components/Navbar/Nav1.tsx
+++ src/components/Navbar/Nav1.tsx
@@ -32,22 +32,31 @@ const Nav: React.FC<NavProps> = ({ scrollToSection }) => {
     const navigate = useNavigate();
 
     const protectedLinks: NavItem[] = [
-        { path: "/home", label: "Home", end: true, icon: <Home /> },
-        { path: "/feed", label: "Feed", end: true, icon: <DynamicFeedOutlinedIcon /> },
-        { path: "/upload", label: "Upload", end: true, icon: <Upload /> },
-        { path: "/profile", label: "Profile", end: true, icon: <AccountBoxOutlinedIcon /> },
+        { path: "/home", label: "Home", end: true,scrollvalue: 0, icon: <Home /> },
+        { path: "/feed", label: "Feed", end: true,scrollvalue: 0, icon: <DynamicFeedOutlinedIcon /> },
+        { path: "/upload", label: "Upload", end: true,scrollvalue: 0, icon: <Upload /> },
+        { path: "/profile", label: "Profile", end: true,scrollvalue: 0, icon: <AccountBoxOutlinedIcon /> },
     ];
 
     const publicLinks: NavItem[] = [
-        { path: "/home", label: "Home", end: true, scrollvalue: 90 },
-        { path: "/home", label: "Featured Discoveries", scrollvalue: 1200 },
-        { path: "/home", label: "How it works", scrollvalue: 2140 },
-        { path: "/home", label: "Recent Discoveries", scrollvalue: 3300 },
-        { path: "/home", label: "Community", scrollvalue: 4600 },
-        { path: "/home", label: "Get Started", scrollvalue: 6000 },
+        { path: "/home", label: "Home", end: true, scrollvalue: 60, icon: "" },
+        { path: "/home", label: "Featured Discoveries", scrollvalue: 900, icon: "" },
+        { path: "/home", label: "How it works", scrollvalue: 1550, icon: "" },
+        // { path: "/home", label: "Recent Discoveries", scrollvalue: 3300, icon: "" },
+        { path: "/home", label: "Community", scrollvalue: 2100, icon: "" },
+        { path: "/home", label: "Get Started", scrollvalue: 2650, icon: "" },
     ];
 
-    const linksToShow = authenticated ? protectedLinks : publicLinks;
+    const publicLinksMobile: NavItem[] = [
+        { path: "/home", label: "Home", end: true, scrollvalue: 90, icon: "" },
+        { path: "/home", label: "Featured Discoveries", scrollvalue: 1200, icon: "" },
+        { path: "/home", label: "How it works", scrollvalue: 2200, icon: "" },
+        // { path: "/home", label: "Recent Discoveries", scrollvalue: 3300, icon: "" },
+        { path: "/home", label: "Community", scrollvalue: 3300, icon: "" },
+        { path: "/home", label: "Get Started", scrollvalue: 4600, icon: "" },
+    ];
+
+    const linksToShow = !authenticated  ? protectedLinks : mobileNavbarOpen? publicLinksMobile:publicLinks;
 
     const handleScroll = () => setScrollPosition(window.scrollY);
 
@@ -67,6 +76,15 @@ const Nav: React.FC<NavProps> = ({ scrollToSection }) => {
             setMobileNavbarOpen(true);
         }
     };
+
+    const handleNavClick = (position: number) => {
+        setIsClosing(true);
+        setTimeout(() => {
+            setMobileNavbarOpen(false);
+            setIsClosing(false);
+            scrollToSection(position);
+        }, 300);
+    }
 
     useEffect(() => {
         window.addEventListener("scroll", handleScroll);
@@ -99,28 +117,26 @@ const Nav: React.FC<NavProps> = ({ scrollToSection }) => {
             clearUserActivityTracking(activityEvents);
             window.removeEventListener("scroll", handleScroll);
         };
-    }, []);
+    }, [mobileNavbarOpen, openMobileNavbarHandler, setMobileNavbarOpen]);
 
     return (
         <div className="navbar-gradient">
             {/* Desktop header */}
             <div className="mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between py-6 navbar-logos">
                 <NavLink to="/home" className="flex items-center space-x-3">
-                    <img src={logo} alt="Logo" className="h-11" />
+                    <img src={logo} alt="company Logo" className="h-11" />
                     <div>
-                        <h1 className="text-xl font-bold text-primary-text">
-                            Inscriptions Platform
-                        </h1>
+                        <h1 className="text-xl font-bold text-primary-text">Inscriptions Platform</h1>
                         <p className="text-sm text-primary-text">C-DAC Bangalore</p>
                     </div>
                 </NavLink>
-                <img src={meityLogo} alt="Logo" className="h-18" />
+                <img src={meityLogo} alt="company Logo" className="h-18" />
             </div>
 
             {/* Mobile header */}
-            <div className="mx-auto px-4 flex items-center justify-between py-6 navbar-logos-mob">
+            <div className="mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between py-6 navbar-logos-mob">
                 <NavLink to="/home" className="flex items-center space-x-3">
-                    <img src={logo} alt="Logo" className="h-9" />
+                    <img src={logo} alt="company Logo" className="h-9" />
                     <div>
                         <p className="font-bold text-primary-text">
                             Inscriptions Platform
@@ -128,40 +144,36 @@ const Nav: React.FC<NavProps> = ({ scrollToSection }) => {
                         <p className="text-sm text-primary-text">C-DAC Bangalore</p>
                     </div>
                 </NavLink>
-                <img src={meityLogo} alt="Logo" className="h-11" />
+                <img src={meityLogo} alt="company Logo" className="h-11" />
             </div>
 
             <hr className="border-t border-white opacity-60" />
-
-            <nav
-                className={`top-0 z-50 navbar-shadow ${
-                    scrollPosition > 200 ? "navscrollbehavior" : ""
-                }`}
-            >
-                <div className="mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="flex items-center justify-between h-16">
-                        {/* Links */}
-                        <div className="flex items-center space-x-4 nav-focus-styling">
-                            {linksToShow.map(({ path, label, end, scrollvalue }) => (
-                                <NavLink
-                                    key={label}
-                                    to={path}
-                                    end={end}
-                                    onClick={() =>
-                                        scrollvalue && scrollToSection(scrollvalue)
-                                    }
-                                    className={({ isActive }) =>
-                                        `hover:text-primary-dark transition-colors ${
-                                            isActive
-                                                ? "text-primary-dark font-semibold"
-                                                : ""
-                                        }`
-                                    }
-                                >
-                                    {label}
-                                </NavLink>
-                            ))}
-
+            <div className="navbar-gradient" style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+                <nav className={`top-0 z-50 text-secondary-text navbar-shadow ${mobileNavbarOpen? (scrollPosition > 2 ? "navscrollbehavior" : ""):scrollPosition > 200 ? "navscrollbehavior" : ""}`} style={{ backgroundColor: "transparent", width:"100%" }} >
+                    <div className=" mx-auto px-4 sm:px-6 lg:px-8">
+                        <div className="flex items-center justify-between h-16 ">
+                            {/* Links */}
+                            <div className="hidden md:flex justify-start space-x-6 header-links header-link-items cursor-pointer items-center nav-focus-styling hide-these-mobile-view" style={{ height: "100%" }}>
+                                {linksToShow.map(({ path, label, end, scrollvalue, icon }) => (
+                                    <NavLink
+                                        key={label}
+                                        to={path}
+                                        end={end}
+                                        onClick={() =>
+                                            scrollToSection(scrollvalue)
+                                        }
+                                        className={({ isActive }) =>
+                                            `text-white transition-colors ${isActive && linksToShow === protectedLinks ? "text-white border-b-4 font-semibold" : ""
+                                            }`
+                                        }
+                                        style={{ height: "95%" }}
+                                    >
+                                        <div className="flex gap-2 justify-center items-center" style={{ height: "100%" }}>
+                                            {icon}{label}
+                                        </div>
+                                    </NavLink>
+                                ))}
+                            </div>
                             {mobileNavbarOpen ? (
                                 <X
                                     className="text-white"
@@ -173,59 +185,63 @@ const Nav: React.FC<NavProps> = ({ scrollToSection }) => {
                                     onClick={openMobileNavbarHandler}
                                 />
                             )}
-                        </div>
 
-                        {/* Auth buttons */}
-                        <div className="flex items-center space-x-3">
-                            {authenticated === null ? (
-                                <CircularProgess />
-                            ) : authenticated ? (
-                                <button
-                                    onClick={() => {
-                                        logout();
-                                        setAuthenticated(false);
-                                        navigate("/login", { replace: true });
-                                    }}
-                                    className="flex items-center gap-2 bg-primary-dark text-white border-2 border-white px-4 py-2 rounded-lg"
-                                >
-                                    <LogOut /> Logout
-                                </button>
-                            ) : (
-                                <button
-                                    onClick={() => navigate("/login")}
-                                    className="flex items-center gap-2 bg-primary-light border-2 border-white px-4 py-2 rounded-lg"
-                                >
-                                    <LogIn /> Login
-                                </button>
-                            )}
+                            {/* Auth buttons */}
+                            <div className="flex justify-between items-center space-x-3" >
+
+                                {authenticated === null ? (
+                                    <CircularProgess />
+                                ) : authenticated ? (
+                                    <button
+                                        onClick={() => {
+                                            logout();
+                                            setAuthenticated(false);
+                                            navigate("/login", { replace: true });
+                                        }}
+                                        className="flex items-center gap-2 bg-primary-dark text-white border-2 border-white cursor-pointer hover:bg-primary/80 pe-4 ps-3 py-2 rounded-lg font-medium transition-colors"
+                                    >
+                                        <LogOut /> Logout
+                                    </button>
+                                ) : (
+                                    <button
+                                        onClick={() => navigate("/login")}
+                                        // className="flex items-center gap-2 bg-primary-light border-2 border-white px-4 py-2 rounded-lg"
+                                        className="flex items-center gap-2 bg-primary-light border-2 border-white cursor-pointer text-primary-text hover:bg-primary/80 pe-4 ps-3 py-2 rounded-lg font-medium transition-colors"
+
+                                    >
+                                        <LogIn /> Login
+                                    </button>
+                                )}
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                {/* Mobile menu */}
-                {mobileNavbarOpen && (
-                    <div
-                        className={`mobile-navbar ${
-                            isClosing ? "mobile-navbar-closing" : ""
-                        }`}
-                    >
-                        {linksToShow.map(({ path, label, end, scrollvalue }) => (
-                            <NavLink
-                                key={label}
-                                to={path}
-                                end={end}
-                                onClick={() => {
-                                    if (scrollvalue)
-                                        scrollToSection(scrollvalue);
-                                    openMobileNavbarHandler();
-                                }}
-                            >
-                                {label}
-                            </NavLink>
-                        ))}
-                    </div>
-                )}
-            </nav>
+                    {/* Mobile menu */}
+                    {mobileNavbarOpen && (
+                        <div
+                            className={`mobile-navbar pt-10 space-y-8 flex flex-col items-center text-white ${isClosing ? "mobile-navbar-closing" : ""
+                                }`}
+                        >
+                            {linksToShow.map(({ path, label, end, scrollvalue, icon }) => (
+                                <NavLink
+                                    key={label}
+                                    to={path}
+                                    end={end}
+                                    onClick={() => handleNavClick(scrollvalue)}
+                                    className={({ isActive }) =>
+                                        `text-white py-4 transition-colors ${isActive && linksToShow === protectedLinks ? "text-white border-b-4 font-semibold" : ""
+                                        }`
+                                    }
+                                >
+                                    <div className="flex gap-2 justify-center items-center" >
+                                        {icon}{label}
+                                    </div>
+                                </NavLink>
+                            ))}
+                        </div>
+                    )}
+                </nav>
+            </div>
         </div>
     );
 };
