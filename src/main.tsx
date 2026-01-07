a1dc3a430cdb951e427ae3df78c0022f24bdc9cf
myusername <sarthaksam003@gmail.com>
2026-01-07 16:51:24 +0530

    Attempt 1: Fixing Mui styling

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,9 @@ import App from './App.tsx';
 import { StyleSheetManager } from 'styled-components';
 import './index.css';
 
-const styleTarget = typeof document !== 'undefined' ? document.head : undefined;
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <StyleSheetManager disableCSSOMInjection target={styleTarget as any}>
+    <StyleSheetManager disableCSSOMInjection>
       <App />
     </StyleSheetManager>
   </StrictMode>
