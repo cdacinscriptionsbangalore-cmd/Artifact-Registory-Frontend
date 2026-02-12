90c7a1bb5db37e04ed18a59189554422100ef0e1
nayan458 <nayancoding@gmail.com>
2026-02-12 09:17:20 +0530

    clean up unused files

--- src/utils/auth.ts
+++
@@ -1,9 +0,0 @@
-import AuthContext from "@/context/AuthContextType";
-import { authStore } from "@/store/authStore";
-import { useContext } from "react";
-
-export const logout = (): void => {
-  authStore.clear();
-  const authCtx = useContext(AuthContext);
-  authCtx.clear();
-};
