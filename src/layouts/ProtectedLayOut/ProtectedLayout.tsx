c961cce9cd5f752f1024f187f3cee24004b4afec
myusername <sarthaksam003@gmail.com>
2026-02-16 10:47:10 +0530

    Restored to working state

--- src/layouts/ProtectedLayOut/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayOut/ProtectedLayout.tsx
@@ -1,5 +1,5 @@
 import AuthContext from '@/context/AuthContext';
-// import { CircularProgress } from '@mui/material';
+import { CircularProgress } from '@mui/material';
 import React, { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 
@@ -10,7 +10,12 @@ interface ProtectedRouteProps {
 export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   const { isAuthenticated, isLoading } = useContext(AuthContext);
 
-  if (isLoading) return null;
+  if (isLoading)
+    return (
+      <div style={{ minHeight: '60vh' }} className="flex items-center justify-center">
+        <CircularProgress />
+      </div>
+    );
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
@@ -26,7 +31,12 @@ interface PublicRouteProps {
 export const PublicRoute: React.FC<PublicRouteProps> = ({ children }) => {
   const { isAuthenticated, isLoading } = useContext(AuthContext);
 
-  if (isLoading) return null;
+  if (isLoading)
+    return (
+      <div style={{ minHeight: '60vh' }} className="flex items-center justify-center">
+        <CircularProgress />
+      </div>
+    );
 
   if (isAuthenticated) {
     return <Navigate to="/home" replace />;
