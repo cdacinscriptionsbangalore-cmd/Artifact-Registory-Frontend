79e84cf4b48a84e94e219b3a8c9aa1c2ecabc113
myusername <sarthaksam003@gmail.com>
2026-02-19 14:44:57 +0530

    Attemp3

--- src/layouts/ProtectedLayOut/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayOut/ProtectedLayout.tsx
@@ -14,7 +14,7 @@ export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
   // outlet area so surrounding layout (including Navbar) remains visible.
   // if (isLoading) return <Navigate to="/login" replace />;
 
-  if (isLoading)
+  if (isLoading )
     return (
       <div style={{ minHeight: "62vh" }}>
         <div className='flex flex items-center justify-center w-100% h-110 flex-col gap-4'>
@@ -43,22 +43,7 @@ export const PublicRoute: React.FC<PublicRouteProps> = ({ children }) => {
 
   // While auth status is being determined, render the public content area (no spinner)
   // so users can sign in while the auth check completes.
-
-
-  if (isLoading) return (
-    <div style={{ minHeight: "62vh" }}>
-      <div className='flex flex items-center justify-center w-100% h-110 flex-col gap-4'>
-        {/* <FaSpinner className="animate-spin text-4xl text-[#66B0FF]" /> */}
-        <img src={cdacRoundLogo} className="mr-3 mb-4 size-20 cdacSpinner" />
-        <div className="text-[#000000] text-lg">
-          Loading...
-        </div>
-      </div>
-    </div>
-  );
-
-
-  if (!isLoading) return <>{children}</>;
+  if (isLoading) return <>{children}</>;
 
   // If user is already authenticated, redirect away from public pages like login
   if (isAuthenticated) {
